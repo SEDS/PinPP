9bd58226a6920154b57582c15dd777db0077a086
James H. Hill <hillj@cs.iupui.edu>
2015-01-27 11:00:29 -0500

    Added more test cases

--- tests/Thread_Test.cpp
+++ tests/Thread_Test.cpp
@@ -14,14 +14,27 @@ class Thread_Test :
 {
 public:
   Thread_Test (void)
-    : thread_test_1_ (0)
+    : thread_start_passed_ (false),
+      already_started_test_passed_ (false),
+      thread_wait_passed_ (false)
   {
     this->enable_fini_callback ();
     this->enable_fini_unlocked_callback ();
 
+    // Create a new Thread and start it.
     cerr << "Starting the thread\n";
     this->thr_ = new OASIS::Pin::Thread (this);
     this->thr_->start ();
+    
+    try
+    {
+      // This should throw an exception
+      this->thr_->start ();
+    }
+    catch (const OASIS::Pin::Thread::Already_Started &)
+    {
+      this->already_started_test_passed_ = true;
+    }
   }
 
   ~Thread_Test (void)
@@ -31,27 +44,31 @@ public:
 
   void handle_fini (INT32 code)
   {
-    cerr << "Test passed: " << (this->thread_test_1_ == 1) << std::endl;
+    cerr << ">> Thread start passed: " << this->thread_start_passed_ << std::endl;
+    cerr << ">> Already started passed: " << this->already_started_test_passed_ << std::endl;
+    cerr << ">> Thread wait passed: " << this->thread_wait_passed_ << std::endl;
   }
 
   void handle_fini_unlocked (INT32 code)
   {
     std::cerr << "Waiting for thread to exit...\n";
 
-    if (!this->thr_->wait (5000))
-      std::cerr << "Time has elapsed" << std::endl;
+    if (this->thr_->wait (5000))
+      this->thread_wait_passed_ = true;
   }
 
   void run (void)
   {
     std::cerr << "Background thread is running..." << std::endl;
-    this->thread_test_1_ = 1;
+    this->thread_start_passed_ = true;
   }
 
 private:
   OASIS::Pin::Thread * thr_;
 
-  int thread_test_1_;
+  bool thread_start_passed_;
+  bool already_started_test_passed_;
+  bool thread_wait_passed_;
 };
 
 DECLARE_PINTOOL (Thread_Test);
