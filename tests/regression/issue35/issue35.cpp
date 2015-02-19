07c55c0578256871c2bc48f4a67defa6a09fedae
James H. Hill <hillj@cs.iupui.edu>
2015-02-19 08:45:52 -0500

    Fixed compilation errors on Linux

--- tests/regression/issue35/issue35.cpp
+++ tests/regression/issue35/issue35.cpp
@@ -12,15 +12,15 @@ class instrument : public OASIS::Pin::Image_Instrument <instrument>
 public:
   instrument (void)
     : count_ (0) { }
-  
+
   void handle_instrument (const OASIS::Pin::Image & img)
   {
-#if defined TARGET_WINDOWS
+#if defined (TARGET_WINDOWS)
     static const std::string ending = "program.exe";
 #else
     static const std::string ending = "program";
 #endif
-    
+
     if (std::equal (ending.rbegin (), ending.rend (), img.name ().rbegin ()))
     {
       for (OASIS::Pin::Section sec : img)
@@ -31,12 +31,12 @@ public:
         }
     }
   }
-  
+
   size_t count (void) const
   {
     return this->count_;
   }
-  
+
 private:
   size_t count_;
 };
@@ -48,32 +48,32 @@ public:
   {
     this->enable_fini_callback ();
   }
-  
+
   void handle_fini (INT32 retcode)
   {
-#if defined TARGET_MAC
+#if defined (TARGET_MAC) || defined (TARGET_LINUX)
     size_t expected = 5;
-#elif defined TARGET_WINDOWS
+#elif defined (TARGET_WINDOWS)
     size_t expected = 75;
 #endif
 
     this->assert_equals (expected, this->instrument_.count (), "Counting number of routines");
   }
-  
+
 private:
   template <typename T>
   void assert_equals (T expected, T actual, const std::string & msg)
   {
     std::cerr << msg << ": ";
-    
+
     if (expected == actual)
       std::cerr << "passed" << std::endl;
     else
       std::cerr << "failed; expected <" << expected << "> but got <" << actual << ">" << std::endl;
   }
-  
+
   instrument instrument_;
-  
+
 };
 
 DECLARE_PINTOOL (issue35)
