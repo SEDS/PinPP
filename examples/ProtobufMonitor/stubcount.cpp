3e6b35a4a1bfd2047c8a7faf90227a00d43dd225
nlui <nlui@iupui.edu>
2019-08-02 16:53:52 -0400

    still cannot get return type by instrumenting image. going back to instrumenting routine because there will be less output to parse through

--- examples/ProtobufMonitor/stubcount.cpp
+++ examples/ProtobufMonitor/stubcount.cpp
@@ -15,10 +15,156 @@
 #include <memory>
 #include <regex>
 
-class Instrument : public OASIS::Pin::Image_Instrument <Instrument>
-{
+template <class T>
+struct return_type;
+
+template <class T, class... Args>
+struct return_type<T(*)(Args...)> {
+	typedef T type;
+};
+
+template <class T, class... Args>
+struct return_type<T(*)(Args..., ...)> {
+	typedef T type;
+};
+
+template<typename T>
+struct type_to_string {
+	static std::string str() {
+		return typeid(T).name();
+	}
+};
+
+template<>
+struct type_to_string<void> {
+	static std::string str() {
+		return std::string("void");
+	}
+};
+
+template<>
+struct type_to_string<short int> {
+	static std::string str() {
+		return std::string("short int");
+	}
+};
+
+template<>
+struct type_to_string<unsigned short int> {
+	static std::string str() {
+		return std::string("unsigned short int");
+	}
+};
+
+template<>
+struct type_to_string<unsigned int> {
+	static std::string str() {
+		return std::string("unsigned int");
+	}
+};
+
+template<>
+struct type_to_string<int> {
+	static std::string str() {
+		return std::string("int");
+	}
+};
+
+template<>
+struct type_to_string<long int> {
+	static std::string str() {
+		return std::string("long int");
+	}
+};
+
+template<>
+struct type_to_string<unsigned long int> {
+	static std::string str() {
+		return std::string("unsigned long int");
+	}
+};
+
+template<>
+struct type_to_string<long long int> {
+	static std::string str() {
+		return std::string("long long int");
+	}
+};
+
+template<>
+struct type_to_string<unsigned long long int> {
+	static std::string str() {
+		return std::string("unsigned long long int");
+	}
+};
+
+template<>
+struct type_to_string<char> {
+	static std::string str() {
+		return std::string("char");
+	}
+};
+
+template<>
+struct type_to_string<signed char> {
+	static std::string str() {
+		return std::string("signed char");
+	}
+};
+
+template<>
+struct type_to_string<unsigned char> {
+	static std::string str() {
+		return std::string("unsigned char");
+	}
+};
+
+template<>
+struct type_to_string<float> {
+	static std::string str() {
+		return std::string("float");
+	}
+};
+
+template<>
+struct type_to_string<double> {
+	static std::string str() {
+		return std::string("double");
+	}
+};
+
+template<>
+struct type_to_string<long double> {
+	static std::string str() {
+		return std::string("long double");
+	}
+};
+
+template<>
+struct type_to_string<bool> {
+	static std::string str() {
+		return std::string("bool");
+	}
+};
+
+template<>
+struct type_to_string<wchar_t> {
+	static std::string str() {
+		return std::string("wchar_t");
+	}
+};
+
+struct func_info {
+	std::string sign;
+	std::string returntype;
+	std::string image;
+};
+
+class Instrument : public OASIS::Pin::Image_Instrument <Instrument> {
 public:
-  typedef std::list <std::string> list_type;
+  typedef std::list <func_info> list_type;
+
+  Instrument(void) { }
 
   void handle_instrument (const OASIS::Pin::Image & img) {
     for (auto sec : img) {
@@ -27,13 +173,22 @@ public:
               continue;
 
             std::string rtn_sign = OASIS::Pin::Symbol::undecorate (rtn.name (), UNDECORATION_COMPLETE);
+	    auto p = rtn.function_ptr();
+	    std::string rtn_returntype = type_to_string<return_type<decltype(p)>::type>::str();
+	    std::string img_name = img.name();
+
+	    func_info info;
+	    info.sign = rtn_sign;
+	    info.returntype = rtn_returntype;
+	    info.image = img_name;
+
             // Add the signature to the listing.
-            this->rtn_signatures_.push_back(rtn_sign);
+            this->rtn_signatures_.push_back(info);
         }
     }
+  }
 
-  const list_type & rtn_signature (void) const
-  {
+  const list_type & rtn_signature (void) const {
     return this->rtn_signatures_;
   }
 
@@ -41,8 +196,7 @@ private:
   list_type rtn_signatures_;
 };
 
-class stubcount : public OASIS::Pin::Tool <stubcount>
-{
+class stubcount : public OASIS::Pin::Tool <stubcount> {
 public:
   stubcount (void)
     : fout_ ("stubcount.json")
@@ -61,15 +215,14 @@ public:
 
     Instrument::list_type inst_list = inst_.rtn_signature();
 
-    for (std::string sign : inst_list) {
-        this->fout_ << "{" << sign << "},";
-    //   //only print info if the procedure has "Stub" and "ClientContext" in the string
-    //   if (std::regex_match(sign, stub_regex)) {
-    //     this->fout_ << "{"
-    //     << "\"Procedure\": \"" << (*iter)->sign << "\","
-    //     << "\"ReturnType\": \"" << (*iter)->return_type << "\","
-    //     << "\"Image\": \"" << (*iter)->image << "\","
-    //   }
+    for (func_info info : inst_list) {
+       //only print info if the procedure has "Stub" and "ClientContext" in the string
+       if (std::regex_match(info.sign, stub_regex)) {
+         this->fout_ << "{"
+         << "\"Procedure\": \"" << info.sign << "\","
+         << "\"ReturnType\": \"" << info.returntype << "\","
+         << "\"Image\": \"" << info.image << "\"}," << std::endl;
+       }
     }
 
     this->fout_ << "]}" << std::endl;
