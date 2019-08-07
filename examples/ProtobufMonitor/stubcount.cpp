7718950ce39a7372334adb2f2fdeeb37711049fd
nlui <nlui@iupui.edu>
2019-08-07 15:30:04 -0400

    Coded instrumentation to detect methods on RPC args

--- examples/ProtobufMonitor/stubcount.cpp
+++ examples/ProtobufMonitor/stubcount.cpp
@@ -12,6 +12,8 @@
 #include <string.h>
 #include <string>
 #include <list>
+#include <vector>
+#include <map>
 #include <memory>
 #include <regex>
 
@@ -43,8 +45,8 @@ class Instrument : public OASIS::Pin::Routine_Instrument <Instrument>
 public:
   Instrument (list_type & out)
     :out_(out),
-    stub_regex("(.*)(Stub::)(.*)(ClientContext)(.*)"),
-    clientctx_regex("(.*)(ClientContext::)(.*)")
+    stub_regex_("(.*)(Stub::)(.*)(ClientContext)(.*)"),
+    clientctx_regex_("(.*)(ClientContext::)(.*)")
   { }
 
   void handle_instrument (const OASIS::Pin::Routine & rtn)
@@ -64,9 +66,12 @@ public:
 
       OASIS::Pin::Routine_Guard guard (rtn);
       methinfo->insert (IPOINT_BEFORE, rtn);
+
+      //extract the args from this RPC method
+      extract_args(sign);
     }
 
-    if (std::regex_match((*iter)->sign_, clientctx_regex)) {
+    if (std::regex_match(sign, clientctx_regex_)) {
       method_info * methinfo = new method_info ();
       methinfo->sign_ = sign;
       methinfo->callee_ = std:string("Client Context");
@@ -77,10 +82,63 @@ public:
       OASIS::Pin::Routine_Guard guard (rtn);
       methinfo->insert (IPOINT_BEFORE, rtn);
     }
+
+    for (const auto &pair : args_) {
+      if (std::regex_match(sign, pair.second)) {
+        method_info * methinfo = new method_info ();
+        methinfo->sign_ = sign;
+        methinfo->callee_ = std:string(pair.first);
+
+        // Add the counter to the listing.
+        this->out_.push_back (methinfo);
+
+        OASIS::Pin::Routine_Guard guard (rtn);
+        methinfo->insert (IPOINT_BEFORE, rtn);
+      }
+    }
   }
+
+//extract_args - used to extract the arguments passed into a method and
+//store in the args list.
+void extract_args(std::string method) {
+    std::vector<std::string::size_type> commas;
+    std::vector<std::string::size_type> substr_lengths;
+
+    //find all comma positions. We don't use string.find() because
+    //we would need to know how many commas are in the string before hand and that requires a second loop through
+    //the string.
+    for (std::string::size_type i=0; i<method.length(); ++i) {
+        if (method[i] == ',') {
+            commas.push_back(i);
+        }
+    }
+
+    for (std::string::size_type i=1; i<commas.size(); ++i) {
+        substr_lengths.push_back(commas[i] - commas[i-1] + 1);
+
+        //we use the location of last comma for the last arg's length
+        if (i == commas.size()-1) {
+            substr_lengths.push_back(commas[i]);
+        }
+    }
+
+    for (std::string::size_type i=0; i<substr_lengths.size(); ++i) {
+        std::string temp = method.substr(commas[i], substr_lengths[i]);
+        temp.replace(0, 2, "");
+        temp.replace(temp.size()-1, 1, "");
+
+        if (args_.count(temp) == 0) {
+            std::string regex_lit("(.*)(::)(.*)");
+            regex_lit.insert(5, temp);
+            std::regex arg_regex(regex_lit);
+            args_.insert(std::make_pair(temp, arg_regex));
+        }
+    }
+}
 
 private:
   list_type & out_;
+  std::map<std::string, std::regex> args_;
 
   //regular expression for finding valid procedures
   //only want procedures that use a Stub followed by a ClientContext, this means
