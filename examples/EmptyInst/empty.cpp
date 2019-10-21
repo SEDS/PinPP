96b58a1dfb42c7721a1b320ce470c995bb32c5b5
nlui <nlui@iupui.edu>
2019-10-21 12:08:19 -0400

    I removed the instrumentation objects and init functions but there is still a massive slowdown with the empty instrument example. Pin/Pin++ must truly be using something that gRPC also uses.

--- examples/EmptyInst/empty.cpp
+++ examples/EmptyInst/empty.cpp
@@ -2,27 +2,18 @@
 #include "pin++/Pintool.h"
 #include <iostream>
 
-class Instrument : public OASIS::Pin::Image_Instrument <Instrument>
-{
-public:
-  void handle_instrument (const OASIS::Pin::Image & img)
-  { }
-};
-
 class empty : public OASIS::Pin::Tool <empty>
 {
 public:
   empty (void)
   {
-    this->init_symbols ();
-    this->enable_fini_callback ();
+    //this->init_symbols ();
+    //this->enable_fini_callback ();
   }
 
   void handle_fini (INT32)
   { }
 
-private:
-  Instrument inst_;
 };
 
 DECLARE_PINTOOL (empty);
