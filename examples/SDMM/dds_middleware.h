151167ce1e490db21ad28462b8609c25849281f8
nlui <nlui@iupui.edu>
2019-11-22 15:35:09 -0500

    Try instrumenting DataWriter.write method instead
    We're going to try instrumenting the DataWriter.write method instead of the marshalling/unmarshalling process

--- examples/SDMM/dds_middleware.h
+++ examples/SDMM/dds_middleware.h
@@ -4,6 +4,9 @@
 #include "middleware.h"
 #include "writer.h"
 
+//Path to ADT definition
+#include "../OpenDDS/DevGuideExamples/DCPS/Messenger/MessengerC.h"
+
 #include <fstream>
 #include <list>
 #include <map>
@@ -21,19 +24,20 @@
 namespace OASIS {
 namespace Pin {
 
-  template<typename ARG2TYPE>
-  class marshaller_info : public OASIS::Pin::Callback <marshaller_info<ARG2TYPE> (OASIS::Pin::ARG_FUNCARG_ENTRYPOINT_VALUE,
+  class write_info : public OASIS::Pin::Callback <write_info (OASIS::Pin::ARG_FUNCARG_ENTRYPOINT_VALUE,
   OASIS::Pin::ARG_FUNCARG_ENTRYPOINT_VALUE)>, public Writer {
   public:
-    marshaller_info (std::string signature, std::string call_object)
-	    :sign_(signature),
-        obj_(call_object),
-        count_(0)
+    write_info (std::string signature, std::string call_object)
+      :sign_(signature),
+      obj_(call_object),
+      count_(0)
     { }
 
     void handle_analyze (ADDRINT arg1, ADDRINT arg2) {
       std::clock_t start = std::clock ();
-      values_.push_back((ARG2TYPE)arg2);
+      Messenger::Message msg;
+      msg = (Messenger::Message const)arg1;
+      values_.push_back(msg);
       ++this->count_;
       std::clock_t end = std::clock ();
       double time = 1000.0 * (end - start) / CLOCKS_PER_SEC;
@@ -44,13 +48,17 @@ namespace Pin {
       out << "{"
       << "\"Values\": [";
 	
-	for (ARG2TYPE &item : this->values_) {
-		out << item << ", ";
-	}
+      for (Messenger::Message &item : this->values_) {
+        out << "{ \"Subject\": \"" << item.subject.in() << "\","
+        << "\"SubjectId\": " << item.subject_id << ","
+        << "\"From\": \"" << item.from.in() << "\","
+        << "\"Count\": " << item.count << ","
+        << "\"Text\": \"" << item.text.in() << "\"},";
+      }
 
       out << "], \"Method\": \"" << this->sign_ << "\","
       << "\"Object\": \"" << this->obj_ << "\","
-      << "\"Call Count\": " << this->count_ << "}"; 
+      << "\"CallCount\": " << this->count_ << "}"; 
     }
 
     virtual bool has_info(void) {
@@ -58,105 +66,16 @@ namespace Pin {
     }
 
   private:
-    std::list<ARG2TYPE> values_;
+    std::list<Messenger::Message> values_;
     std::string sign_;
     std::string obj_;
-	UINT64 count_;
-  };
-
-  template<>
-  class marshaller_info <const char*> : public OASIS::Pin::Callback <marshaller_info<const char*> (OASIS::Pin::ARG_FUNCARG_ENTRYPOINT_VALUE,
-  OASIS::Pin::ARG_FUNCARG_ENTRYPOINT_VALUE)>, public Writer {
-  public:
-    marshaller_info (std::string signature, std::string call_object)
-	    :sign_(signature),
-        obj_(call_object),
-        count_(0)
-    { }
-
-    void handle_analyze (ADDRINT arg1, ADDRINT arg2) {
-      std::clock_t start = std::clock ();
-      values_.push_back(std::string((const char*)arg2));
-      ++this->count_;
-      std::clock_t end = std::clock ();
-      double time = 1000.0 * (end - start) / CLOCKS_PER_SEC;
-      accum_meth_info.increase(time);
-    }
-
-    virtual void write_to(std::ostream & out) {
-      out << "{"
-      << "\"Values\": [";
-
-	for (std::string &item : this->values_) {
-		out << "\"" << item << "\", ";
-	}
-
-      out << "], \"Method\": \"" << this->sign_ << "\","
-      << "\"Object\": \"" << this->obj_ << "\","
-      << "\"Call Count\": " << this->count_ << "}"; 
-    }
-
-    virtual bool has_info(void) {
-      return count_;
-    }
-
-  private:
-    std::list<std::string> values_;
-    std::string sign_;
-    std::string obj_;
-	UINT64 count_;
-  };
-
-  class credentials_info : public OASIS::Pin::Callback <credentials_info (OASIS::Pin::ARG_FUNCARG_ENTRYPOINT_VALUE,
-  OASIS::Pin::ARG_FUNCARG_ENTRYPOINT_VALUE,
-  OASIS::Pin::ARG_FUNCARG_ENTRYPOINT_VALUE,
-  OASIS::Pin::ARG_FUNCARG_ENTRYPOINT_VALUE)>, public Writer {
-  public:
-    credentials_info (std::string signature, std::string call_object)
-	    :port_(0),
-	publisher_address_("sample.address.edu"),
-	sign_(signature),
-        obj_(call_object),
-        count_(0)
-    { }
-
-    void handle_analyze (ADDRINT arg1, ADDRINT arg2, ADDRINT arg3, ADDRINT arg4) {
-      std::clock_t start = std::clock ();
-      port_ = (unsigned short)arg1;
-      //publisher_address_.assign((char const*)arg2);
-      ++this->count_;
-      std::clock_t end = std::clock ();
-      double time = 1000.0 * (end - start) / CLOCKS_PER_SEC;
-      accum_meth_info.increase(time);
-    }
-
-    virtual void write_to(std::ostream & out) {
-      out << "{"
-      << "\"Port\": " << this->port_ << ","
-      << "\"Publisher Address\": \"" << this->publisher_address_ << "\","
-      << "\"Method\": \"" << this->sign_ << "\","
-      << "\"Object\": \"" << this->obj_ << "\","
-      << "\"Call Count\": " << this->count_ << "}"; 
-    }
-
-    virtual bool has_info(void) {
-      return count_;
-    }
-
-  private:
-    unsigned short port_;
-    std::string publisher_address_;
-    std::string sign_;
-    std::string obj_;
-	UINT64 count_;
+    UINT64 count_;
   };
 
   class DDS_Middleware : public Middleware {
   public:
     DDS_Middleware(std::vector<std::string> & method_list, std::string & obv)
-      :ace_inet_set_("ACE_INET_Addr::set(unsigned short, char const*, int, int)"),
-      marshaller_string_("OpenDDS::DCPS::operator<<(OpenDDS::DCPS::Serializer&, char const*)"),
-      marshaller_int_("OpenDDS::DCPS::operator<<(OpenDDS::DCPS::Serializer&, int)")
+      :datawriter_write_("write(Messenger::Message const&, int)")
     {  }
 
     virtual std::string name(void) {
@@ -179,34 +98,14 @@ namespace Pin {
       std::string calling_object("N/A");
 
       //does the signature match one of the substrings we're looking for?
-      if (signature.find(ace_inet_set_) != std::string::npos) {
-        calling_object = std::string("ACE_INET_Addr");
+      if (signature.find(datawriter_write_) != std::string::npos) {
+        calling_object = std::string("OpenDDS::DCPS::DataWriterImpl_T<Messenger::Message>");
 
-        credentials_info *c_info = new credentials_info(signature, calling_object);
-        this->output_list_.push_back((Writer *) c_info);
-
-        OASIS::Pin::Routine_Guard guard (rtn);
-        c_info->insert (IPOINT_BEFORE, rtn, 0, 1, 2, 3);
-      }
-
-      if (signature.find(marshaller_string_) != std::string::npos) {
-        calling_object = std::string("OpenDDS::DCPS");
-
-        marshaller_info<const char*> *m_info = new marshaller_info<const char*> (signature, calling_object);
-        this->output_list_.push_back ((Writer *) m_info);
+        write_info *w_info = new write_info(signature, calling_object);
+        this->output_list_.push_back ((Writer *) w_info);
 
         OASIS::Pin::Routine_Guard guard (rtn);
-        m_info->insert (IPOINT_BEFORE, rtn, 0, 1);
-      }
-
-      if (signature.find(marshaller_int_) != std::string::npos) {
-        calling_object = std::string("OpenDDS::DCPS");
-
-        marshaller_info<int> *m_info = new marshaller_info<int> (signature, calling_object);
-        this->output_list_.push_back ((Writer *) m_info);
-
-        OASIS::Pin::Routine_Guard guard (rtn);
-        m_info->insert (IPOINT_BEFORE, rtn, 0, 1);
+        w_info->insert (IPOINT_BEFORE, rtn, 0, 1);
       }
     }
 
@@ -215,13 +114,10 @@ namespace Pin {
     }
   private:
     //output_list_ - the output list
-    //ace_inet_addre_ - substring to identify set method for the remote address and port number
-    //marshaller_string_ - substring to identify the << method used to marshall C-strings
-    //marshaller_int_ - substring to identify the << method use to marshall integers
+    //datawriter_write_ - substring to identify the DataWriter.write() method used to publish data to a
+    //Topics repository.
     list_type output_list_;
-    std::string ace_inet_set_;
-    std::string marshaller_string_;
-    std::string marshaller_int_;
+    std::string datawriter_write_;
   };
 }
 }
