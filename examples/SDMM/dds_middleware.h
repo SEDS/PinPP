b667ee30f40a4a914371e65b0b0b3223d4a24fe9
nlui <nlui@iupui.edu>
2019-12-05 15:07:41 -0500

    Determine the topics for the DDS app
    In DDS there are things called Topics which are the repositories that hold all the data communicated between Publisher/Subscriber. Let's extract their names in our monitor.

--- examples/SDMM/dds_middleware.h
+++ examples/SDMM/dds_middleware.h
@@ -21,9 +21,9 @@
 namespace OASIS {
 namespace Pin {
 
-  class dds_method_info : public OASIS::Pin::Callback <dds_method_info (void)>, public Writer {
+  class dds_readwrite_info : public OASIS::Pin::Callback <dds_readwrite_info (void)>, public Writer {
   public:
-    dds_method_info (std::string signature, std::string call_object)
+    dds_readwrite_info (std::string signature, std::string call_object)
       : sign_(signature),
       obj_(call_object),
       count_(0)
@@ -55,11 +55,53 @@ namespace Pin {
 	  UINT64 count_;
   };
 
+  class dds_topic_info : public OASIS::Pin::Callback <dds_topic_info (OASIS::Pin::ARG_FUNCARG_ENTRYPOINT_VALUE,
+  OASIS::Pin::ARG_FUNCARG_ENTRYPOINT_VALUE,
+  OASIS::Pin::ARG_FUNCARG_ENTRYPOINT_VALUE,
+  OASIS::Pin::ARG_FUNCARG_ENTRYPOINT_VALUE,
+  OASIS::Pin::ARG_FUNCARG_ENTRYPOINT_VALUE)>, public Writer {
+  public:
+    dds_topic_info (std::string signature, std::string call_object)
+      : sign_(signature),
+      obj_(call_object),
+      count_(0)
+    { }
+
+    void handle_analyze (ADDRINT arg1, ADDRINT arg2, ADDRINT arg3, ADDRINT arg4, ADDRINT arg5) {
+      std::clock_t start = std::clock ();
+      ++this->count_;
+      this->topic_name_ = (char const*)arg1;
+      std::clock_t end = std::clock ();
+      double time = 1000.0 * (end - start) / CLOCKS_PER_SEC;
+      accum_meth_info.increase(time);
+    }
+
+    virtual void write_to(std::ostream & out) {
+      if (count_ > 0) {
+        out << "{"
+        << "\"Method\": \"" << this->sign_ << "\","
+        << "\"Object\": \"" << this->obj_ << "\","
+        << "\"Topic Name\": \"" << this->topic_name_ << "\","
+        << "\"Call Count\": \"" << this->count_ << "\"}";
+      }
+    }
+
+    virtual bool has_info(void) {
+      return count_;
+    }
+  private:
+    std::string topic_name_;
+    std::string sign_;
+    std::string obj_;
+	  UINT64 count_;
+  };
+
   class DDS_Middleware : public Middleware {
   public:
     DDS_Middleware(std::vector<std::string> & method_list, std::string & obv)
       :datawriter_write_("::write("),
-      datareader_takenextsample_("::take_next_sample(")
+      datareader_takenextsample_("::take_next_sample("),
+      create_topic_("::create_topic(char const*, char const*, DDS::TopicQos const&, DDS::TopicListener*, unsigned int)")
     {  }
 
     virtual std::string name(void) {
@@ -85,7 +127,7 @@ namespace Pin {
       if (signature.find(datawriter_write_) != std::string::npos && signature.find("DataWriterImpl_T") != std::string::npos) {
         calling_object = std::string("OpenDDS::DCPS::DataWriterImpl_T<Messenger::Message>");
 
-        dds_method_info * dds_info = new dds_method_info(signature, calling_object);
+        dds_readwrite_info * dds_info = new dds_readwrite_info(signature, calling_object);
         this->output_list_.push_back((Writer *) dds_info);
 
         OASIS::Pin::Routine_Guard guard (rtn);
@@ -95,11 +137,21 @@ namespace Pin {
       if (signature.find(datareader_takenextsample_) != std::string::npos) {
         calling_object = std::string("OpenDDS::DCPS::DataReaderImpl_T<Messenger::Message>");
 
-        dds_method_info * dds_info = new dds_method_info(signature, calling_object);
+        dds_readwrite_info * dds_info = new dds_readwrite_info(signature, calling_object);
         this->output_list_.push_back((Writer *) dds_info);
 
         OASIS::Pin::Routine_Guard guard (rtn);
         dds_info->insert (IPOINT_BEFORE, rtn);
+      }
+
+      if (signature.find(create_topic_) != std::string::npos) {
+        calling_object = std::string("OpenDDS::DCPS::DomainParticipantImpl");
+
+        dds_topic_info * topic_info = new dds_topic_info(signature, calling_object);
+        this->output_list_.push_back((Writer *) topic_info);
+
+        OASIS::Pin::Routine_Guard guard (rtn);
+        topic_info->insert (IPOINT_BEFORE, rtn, 0, 1, 2, 3, 4);
       }
     }
 
