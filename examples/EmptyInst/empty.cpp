b5b1499c257a3fb217f50d76431c3bc27bc47a30
nlui <nlui@iupui.edu>
2019-12-17 17:35:47 -0500

    traditional pin works with primitive in parameter list. now let's try with structs and strings

--- examples/EmptyInst/empty.cpp
+++ examples/EmptyInst/empty.cpp
@@ -1,19 +1,5 @@
 #include "pin.H"
 #include <iostream>
-#include <fstream>
-
-/* ===================================================================== */
-/* Global Variables */
-/* ===================================================================== */
-
-std::ofstream TraceFile;
-
-/* ===================================================================== */
-/* Commandline Switches */
-/* ===================================================================== */
-
-KNOB<string> KnobOutputFile(KNOB_MODE_WRITEONCE, "pintool",
-    "o", "trace.out", "specify trace file name");
 
 /* ===================================================================== */
 
@@ -24,10 +10,10 @@ KNOB<string> KnobOutputFile(KNOB_MODE_WRITEONCE, "pintool",
  
 VOID HandleNyalia(CHAR * name, int a, int b, int c)
 {
-  TraceFile << name << " | ";
-  TraceFile << a << " | ";
-  TraceFile << b << " | ";
-  TraceFile << c << std::endl;
+  std::cout << name << " | ";
+  std::cout << a << " | ";
+  std::cout << b << " | ";
+  std::cout << c << std::endl;
 }
 
 
@@ -37,13 +23,13 @@ VOID HandleNyalia(CHAR * name, int a, int b, int c)
    
 VOID Image(IMG img, VOID *v)
 {
-    RTN nyalia_rtn = RTN_FindByName(img, "Nyalia");
+    RTN nyalia_rtn = RTN_FindByName(img, "_Z6Nyaliaiii");
     
     if (RTN_Valid(nyalia_rtn))
     {
         RTN_Open(nyalia_rtn);
         RTN_InsertCall(nyalia_rtn, IPOINT_BEFORE, (AFUNPTR)HandleNyalia,
-                       IARG_ADDRINT, "Nyalia",
+                       IARG_ADDRINT, "_Z6Nyaliaiii",
                        IARG_FUNCARG_ENTRYPOINT_VALUE, 0,
                        IARG_FUNCARG_ENTRYPOINT_VALUE, 1,
                        IARG_FUNCARG_ENTRYPOINT_VALUE, 2,
@@ -53,20 +39,12 @@ VOID Image(IMG img, VOID *v)
 }
 
 /* ===================================================================== */
-
-VOID Fini(INT32 code, VOID *v)
-{
-    TraceFile.close();
-}
-
-/* ===================================================================== */
 /* Print Help Message                                                    */
 /* ===================================================================== */
    
 INT32 Usage()
 {
     std::cerr << "This tool is used for testing PIN and Pin++" << std::endl;
-    std::cerr << std::endl << KNOB_BASE::StringKnobSummary() << std::endl;
     return -1;
 }
 
@@ -83,14 +61,8 @@ int main(int argc, char *argv[])
         return Usage();
     }
     
-    // Write to a file since cout and cerr maybe closed by the application
-    TraceFile.open(KnobOutputFile.Value().c_str());
-    TraceFile << hex;
-    TraceFile.setf(ios::showbase);
-    
     // Register Image to be called to instrument functions.
     IMG_AddInstrumentFunction(Image, 0);
-    PIN_AddFiniFunction(Fini, 0);
 
     // Never returns
     PIN_StartProgram();
